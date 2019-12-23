e51aefd905ae9f6a9e0d73811567b412a1a0cc7c
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2019-12-23 22:32:03 +0000

    issue with var name

--- cloud-init.tf
+++ cloud-init.tf
@@ -21,7 +21,7 @@ data template_cloudinit_config express {
       nginx_content = base64gzip(templatefile("${path.module}/templates/nginx.conf", {
         sql_server_fqdn = var.sql_server_fqdn
         sql_server_port = var.sql_server_port
-        timeout         = var.connection_timout
+        timeout         = var.connection_timeout
       }))
       html_content = base64gzip(templatefile("${path.module}/templates/index.html", {
         name = var.name
