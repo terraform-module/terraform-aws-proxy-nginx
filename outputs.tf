66c4aa154933f9e3b9218d0397b7b3067364c485
Will Poynter <will@tintoapp.com>
2020-12-13 20:03:40 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "public_ip" {
   description = "IP addresses assigned to the instance, if applicable"
   value       = module.this.public_ip
 }
+  
+output "public_dns" {
+  description = "Public DNS names assigned to the instance, if applicable"
+  value       = module.this.public_dns
+}
 
 output "id" {
   description = "The ID of the instance"
